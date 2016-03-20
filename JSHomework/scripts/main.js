7a97de8a00c22d03ce311166740084f06760f079
IvelinaBorisova <borisovaivelina@gmail.com>
2016-03-20 15:48:03 +0200

    Added files via upload

---
+++ JSHomework/scripts/main.js
@@ -0,0 +1,21 @@
+var numb = 16;                               
+var string1 = "Johnson"; 
+var arr = [12, 321, 232];           
+var obj = {
+			name: "Ivelina",
+			age: 21,
+		};   
+
+var boo = true;
+var person = undefined;
+var dNull = null ;
+var dSymb = Symbol("aaa");
+
+console.log(typeof(numb));
+console.log(typeof(string1));
+console.log(typeof(arr));
+console.log(typeof(obj));
+console.log(typeof(boo));
+console.log(typeof(person));
+console.log(typeof(dNull)); // typeof reports null as object
+console.log(typeof(dSymb));
---
+++ JSHomework/scripts/script.js
@@ -0,0 +1,27 @@
+var a = 30;
+var b = 50;
+
+var dog = {
+		legs:4,
+		furColor:'black',
+		bark : function(){
+		return "bau-bau";}
+
+		};
+var r = Math.random(0,9);
+var cars = ['fiat', 'bmw', 'mercedes' ];
+
+function multi(x,y)
+{
+return (x*y);
+}
+xy=multi(a,b);
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(dog.bark());
+console.log(dog);
+console.log(r);
+console.log(cars);
+console.log(xy);
